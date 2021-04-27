244c2d0ed6c7048abe63c66b29f27150ee1c797f
Ilya Mihov <mixov2000@gmail.com>
2021-04-27 21:31:18 +0300

    Started Login Form UI

---
+++ Software Accounting/LoginForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Software_Accounting
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonLogin = new Software_Accounting.Resources.Classes.CircularButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelRegister = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBoxTitle = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.buttonLogin);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.labelRegister);
+            this.panel1.Controls.Add(this.buttonExit);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(353, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 556);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
+            this.buttonLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
+            this.buttonLogin.FlatAppearance.BorderSize = 0;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.Location = new System.Drawing.Point(70, 360);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Padding = new System.Windows.Forms.Padding(5);
+            this.buttonLogin.Size = new System.Drawing.Size(411, 69);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Войти";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(244, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Вход";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelRegister
+            // 
+            this.labelRegister.AutoSize = true;
+            this.labelRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelRegister.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.labelRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(194)))), ((int)(((byte)(255)))));
+            this.labelRegister.Location = new System.Drawing.Point(226, 512);
+            this.labelRegister.Name = "labelRegister";
+            this.labelRegister.Size = new System.Drawing.Size(100, 21);
+            this.labelRegister.TabIndex = 4;
+            this.labelRegister.Text = "Регистрация";
+            this.labelRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelRegister.Click += new System.EventHandler(this.labelRegister_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.buttonExit.Location = new System.Drawing.Point(510, 0);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(35, 35);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "X";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(70, 294);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Пароль";
+            this.textBox2.Size = new System.Drawing.Size(411, 16);
+            this.textBox2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Aqua;
+            this.panel3.Location = new System.Drawing.Point(70, 312);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(411, 1);
+            this.panel3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(70, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Почта";
+            this.textBox1.Size = new System.Drawing.Size(411, 16);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Aqua;
+            this.panel2.Location = new System.Drawing.Point(70, 244);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(411, 1);
+            this.panel2.TabIndex = 0;
+            // 
+            // richTextBoxTitle
+            // 
+            this.richTextBoxTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.richTextBoxTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxTitle.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBoxTitle.ForeColor = System.Drawing.Color.White;
+            this.richTextBoxTitle.Location = new System.Drawing.Point(23, 158);
+            this.richTextBoxTitle.Name = "richTextBoxTitle";
+            this.richTextBoxTitle.ReadOnly = true;
+            this.richTextBoxTitle.Size = new System.Drawing.Size(326, 210);
+            this.richTextBoxTitle.TabIndex = 1;
+            this.richTextBoxTitle.Text = "Приложение\nдля учета программных\nпродуктов";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 521);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ООО \"Партнер Ай Ди\"";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(897, 556);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBoxTitle);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вход";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBoxTitle;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelRegister;
+        private System.Windows.Forms.Label label1;
+        private Resources.Classes.CircularButton buttonLogin;
+    }
+}
+
